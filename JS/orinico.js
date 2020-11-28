cb6b9209873e327b0cf9999d6eba7c5d5268d3c8
babajavascript <baptiste.javascript@gmail.com>
2020-11-28 23:42:53 +0100

    main page request API script

--- JS/orinico.js
+++ JS/orinico.js
@@ -1,10 +1,3 @@
-var request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-    }
-}
-request.open("GET", "http://localhost:3000/api/cameras");
-request.send();
 
-/////////////////////////////////////////////////////////////////////////
+
+
